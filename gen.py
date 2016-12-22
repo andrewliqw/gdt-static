5e1ca0b0582ab7f9cbe2a6928809da334612877f
Andrew Li <andrewliqw@outlook.com>
2016-12-22 18:09:21 +1100

    make gen.py support subdirectory

--- gen.py
+++ gen.py
@@ -14,7 +14,20 @@ parser.add_argument(
     help="specify the template filename e.g.: index_t.html"
 )
 
+parser.add_argument(
+    "static_level",
+    type=int,
+    help="level of static directory relative to template file"
+)
+
 args = parser.parse_args()
+
+if args.static_level == 0:
+    static_url = "."
+elif args.static_level > 0:
+    static_url = "../" * args.static_level
+else:
+    raise ValueError("level should be >=0")
 
 args.template_file = os.path.abspath(args.template_file)
 
@@ -23,7 +36,8 @@ django_engine = Engine(
 )
 
 context = Context({
-    "STATIC_URL": os.path.join("./static", ""),
+    "STATIC_URL": os.path.join(static_url, "static", ""),
+    # CURRENT_FILE used for navbar
     "CURRENT_FILE": os.path.basename(args.template_file),
 })
 print(django_engine.get_template(args.template_file).render(context))
